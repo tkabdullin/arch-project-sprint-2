e5321b4b418ca0ca9a89253a77307d6eef55e79d
Дуюнов Андрей <Duyunov.AS@gazprom-neft.ru>
2024-07-27 00:49:14 +0300

    Задание 2. Шардирование

---
+++ mongo-sharding/scripts/tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###
+# Тест бд
+###
+
+docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+use somedb;
+db.helloDoc.countDocuments();
+EOF
+
+
+docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+use somedb;
+db.helloDoc.countDocuments();
+EOF
