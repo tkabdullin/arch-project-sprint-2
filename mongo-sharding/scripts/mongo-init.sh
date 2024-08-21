a311aaf0e3a209d2c72af44f43afa410f2248fd9
mnemchenko <mnemchenko@gmail.com>
2024-08-22 02:04:38 +0400

    task 2 has been finished

---
+++ mongo-sharding/scripts/mongo-init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+docker exec -it mongo-sharding-configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+EOF
+
+docker exec -it mongo-sharding-shard1 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" }
+      ]
+    }
+);
+EOF
+
+docker exec -it mongo-sharding-shard2 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+);
+EOF
+
+docker exec -it mongo-sharding-mongos_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+EOF
