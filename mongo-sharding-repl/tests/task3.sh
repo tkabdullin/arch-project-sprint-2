575da77ddab0987303a1e25d6ea2ea3f59a471b6
Ivan Mashin <ismashin@sberbank.ru>
2024-09-03 01:27:39 +0300

    complete task 3

---
+++ mongo-sharding-repl/tests/task3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+docker compose exec -T shard1-1 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+
+docker compose exec -T shard1-2 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+
+docker compose exec -T shard1-3 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+
+
+docker compose exec -T shard2-1 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+
+docker compose exec -T shard2-2 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
+
+docker compose exec -T shard2-3 mongosh --port 27018 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
